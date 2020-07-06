6fdd6df130162963a83b63a81c1a5e3c9b1e678d
Ali Hadi <ozturkalihadi@gmail.com>
2020-07-06 22:22:09 +0300

    Added Firabase cloud messaging and ignored google-service.json

---
+++ SelfTracker.Android/Firebase/MyFirebaseIIDService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Firebase.Iid;
+
+namespace SelfTracker.Droid.Firebase
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENT" })]
+    [Obsolete]
+    public class MyFirebaseIIDService : FirebaseInstanceIdService
+    {
+        const string TAG = "MyFirebaseIIDService";
+
+        public override void OnTokenRefresh()
+        {
+            var refreshedToken = FirebaseInstanceId.Instance.Token;
+            Log.Debug(TAG, "Refreshed token: " + refreshedToken);
+            SendRegistrationToServer(refreshedToken);
+        }
+
+        void SendRegistrationToServer(string token)
+        {
+            // Add custom implementation, as needed.
+        }
+    }
+}
--- SelfTracker.Android/MainActivity.cs
+++ SelfTracker.Android/MainActivity.cs
@@ -1,20 +1,44 @@
 ﻿using System;
 using Android.App;
-using Android.Content.PM;
+using Android.Content;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Gms.Common;
+using Android.Util;
+using Firebase.Iid;
+using Firebase.Messaging;
+using Android.Media;
+using Android.Content.PM;
 
 namespace SelfTracker.Droid
 {
     [Activity(Label = "SelfTracker", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        const string TAG = "MainActivity";
+        internal static readonly string CHANNEL_ID = "my_notification_channel";
+        internal static readonly int NOTIFICATION_ID = 100;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
+
+            #region FCM
+            CreateNotificationChannel();
+            IsPlayServicesAvailable();
+
+
+            Log.Debug(TAG, "InstanceID token: " + FirebaseInstanceId.Instance.Id);
+
+            FirebaseMessaging.Instance.SubscribeToTopic("news");
+            Log.Debug(TAG, "Subscribed to remote notifications");
+
+            #endregion
+
+
+
 
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
@@ -26,6 +50,56 @@ namespace SelfTracker.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public bool IsPlayServicesAvailable()
+        {
+            var resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                {
+                    //msgText.Text = GoogleApiAvailability.Instance.GetErrorString(resultCode);
+                }
+                else
+                {
+                    //msgText.Text = "This device is not supported";
+                    Finish();
+                }
+
+                return false;
+            }
+
+            //msgText.Text = "Google Play Services is available.";
+            return true;
+        }
+
+
+        void CreateNotificationChannel()
+        {
+            if (Build.VERSION.SdkInt < BuildVersionCodes.O)
+            {
+                // Notification channels are new in API 26 (and not a part of the
+                // support library). There is no need to create a notification 
+                // channel on older versions of Android.
+                return;
+            }
+
+            var alarmAttributes = new AudioAttributes.Builder()
+                    .SetContentType(AudioContentType.Sonification)
+                    .SetUsage(AudioUsageKind.Notification).Build();
+
+            // Create the uri for the alarm file                 
+            Android.Net.Uri alarmUri = Android.Net.Uri.Parse($"{ContentResolver.SchemeAndroidResource}://{Application.Context.PackageName}/{Resource.Raw.soundFile}");
+
+            var channel = new NotificationChannel(CHANNEL_ID, "SelfTrackerNotificationChannel", NotificationImportance.Default)
+            {
+                Description = "Notification Channel"
+            };
+            channel.SetSound(alarmUri, alarmAttributes);
+
+            var notificationManager = (NotificationManager)GetSystemService(NotificationService);
+            notificationManager.CreateNotificationChannel(channel);
         }
     }
 }
